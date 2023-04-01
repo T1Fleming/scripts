316e2fd6cfc75010287f0e83e3d75075c4cf66f8
t1sizzle <eric.fleming101@gmail.com>
2023-04-01 14:39:53 -0700

    init

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-const compare = require('./compare')
-const obj1 = require("./inputs/a.json")
-const obj2 = require("./inputs/b.json")
-
-
+const compare = require('./compare/compare')
+const obj1 = require("./compare/inputs/a.json")
+const obj2 = require("./compare/inputs/b.json")
 
 const res = compare.traverse([], obj1, obj2)
 console.log(res)
